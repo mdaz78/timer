fcf3d22bbc505007326aeee39e67a42c24d460a6
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-09-20 18:15:09 +0530

    refactor!: Move timer class in a different file

--- app.js
+++ app.js
@@ -1,49 +1,5 @@
 "use strict";
 
-class Timer {
-  constructor(durationInput, startButton, pauseButton, callbacks) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    if (callbacks) {
-      const { onStart, onTick, onComplete } = callbacks;
-      this.onStart = onStart;
-      this.onTick = onTick;
-      this.onComplete = onComplete;
-    }
-
-    this.startButton.addEventListener("click", this.start);
-    this.pauseButton.addEventListener("click", this.pause);
-  }
-
-  start = () => {
-    this.onStart && this.onStart();
-    this.tick();
-    this.intervalId = setInterval(this.tick, 1000);
-  };
-
-  pause = () => {
-    clearInterval(this.intervalId);
-  };
-
-  tick = () => {
-    if (this.timeRemaining <= 0) {
-      this.pause();
-    } else {
-      this.timeRemaining = this.timeRemaining - 1;
-    }
-  };
-
-  get timeRemaining() {
-    return parseFloat(this.durationInput.value);
-  }
-
-  set timeRemaining(time) {
-    this.durationInput.value = time;
-  }
-}
-
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
---
+++ timer.js
@@ -0,0 +1,43 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    if (callbacks) {
+      const { onStart, onTick, onComplete } = callbacks;
+      this.onStart = onStart;
+      this.onTick = onTick;
+      this.onComplete = onComplete;
+    }
+
+    this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
+  }
+
+  start = () => {
+    this.onStart && this.onStart();
+    this.tick();
+    this.intervalId = setInterval(this.tick, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.intervalId);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+}
